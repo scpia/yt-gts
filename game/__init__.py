5604beccdcfbc0286cc0cb2bc74b359381742479
finn <finaltos@gmail.com>
2024-08-29 17:03:42 +0200

    started templates and youtube-lyrics

--- game/__init__.py
+++ game/__init__.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from youtube_lyrics import get_lyrics
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+
+@app.route('/get_lyrics', methods=['GET'])
+def get_lyrics():
+    return get_lyrics()
